293431826d98068ea5012ea6e4d787ae1530649e
Jharna203 <jharnakhatun203@gmail.com>
2022-12-14 01:07:42 +0600

    added Home browse title

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useTitle from '../../Hook/useTitle';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Projects from '../Projects/Projects';
 import Hero from './Hero';
 
 const Home = () => {
+    useTitle("Home");
     return (
         <div>
             <Hero className="w-full h-screen"></Hero>
