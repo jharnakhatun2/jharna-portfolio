f579323cb379ede1d7c497f2e338f98676cfcb5a
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-06-18 10:35:47 +0600

    update about me section

---
+++ src/Componects/HeaderText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderText = () => {
+  return (
+    <div>HeaderText</div>
+  )
+}
+
+export default HeaderText
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,40 +1,38 @@
 import React from "react";
 import useTitle from "../../Hook/useTitle";
+import { BsArrowRight } from "react-icons/bs";
 
 const About = () => {
   useTitle("About");
   return (
-    <div className="my-20">
-      <div className="text-center ">
-        <h1 className="text-4xl font-bold">ABOUT ME</h1>
-        <div className="divider w-2/12 mx-auto">
-          <p className="">JS</p>
-        </div>
-      </div>
+    <div className="py-10 bg-black">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 m-8 lg:m-20">
         <div>
-          <h1 className="text-2xl font-bold">GET TO KNOW ME :</h1>
-          <p className="my-7 text-lg text-justify text-slate-600">
+          <p className="text-[#357e8a] font-bold mb-1 border w-fit px-2 border-slate-600">who I am</p>
+          <h1 className="text-2xl font-bold text-gray-300">ABOUT ME</h1>
+          <div className="divider w-1/12 my-2"></div>
+          <p className="mt-4 mb-7 text-lg text-justify text-gray-400">
             I'm a MERN Stack Web Developer building different types of core
             javaScript projects, React js web applications and full stack web
             applications. Check out some of my work in the Projects section.
-            <br />
+            <br /><br />
             I also like to work with group works where I can share my knowledge
             to the stuff that I have learned over the years in Web Development
             so it can help other people of the Dev Community.Also I am a quick
             learner about any upcoming update technology.
-            <br />
+            <br /><br />
             I'm open to Job opportunities where I can contribute, learn and
             grow. If you have a good opportunity that matches my skills and
             experience then don't hesitate to contact me.
           </p>
-          <button className="btn btn-secondary shadow-lg w-full lg:w-1/3">
-            <a href="#contact">CONTACT</a>{" "}
+          <button className="btn btn-hover btn-secondary shadow-lg w-9/12 mx-auto lg:w-1/3">
+            <a href="/contact" className="flex">CONTACT <span className="ml-3"><BsArrowRight/></span></a>{" "}
           </button>
         </div>
         <div>
-          <h1 className="text-2xl font-bold mb-7">MY SKILLS :</h1>
-          <div className="grid grid-cols-3 lg:grid-cols-4 gap-5">
+          <h1 className="text-2xl font-bold text-gray-300">MY SKILLS</h1>
+          <div className="divider w-1/12 my-2"></div>
+          <div className="grid grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
             <div className="text-center p-3 border rounded text-accent">
               HTML
             </div>
